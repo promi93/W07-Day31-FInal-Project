cb0fa882d3733c2a1ef9f406a9cfc0a737b6fee5
promi93 <116350522+promi93@users.noreply.github.com>
2023-03-21 15:59:52 +0100

    change responsive

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,17 +1,16 @@
 // RICHIAMO DELL' API E URL
 const myApi = "https://striveschool-api.herokuapp.com/api/product/";
 const myAuth =
-"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0NTc0MWY4MWI0MjAwMTM5YjI5ZGIiLCJpYXQiOjE2NzkzNTA2MTEsImV4cCI6MTY4MDU2MDIxMX0.M0DDg2Bpb4XuDRHfiIhw1hSC_B4v4a7-2cyXDdL8jUE";
-
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE0NTc0MWY4MWI0MjAwMTM5YjI5ZGIiLCJpYXQiOjE2NzkzNTA2MTEsImV4cCI6MTY4MDU2MDIxMX0.M0DDg2Bpb4XuDRHfiIhw1hSC_B4v4a7-2cyXDdL8jUE";
 
 // Dichiaro variabile per input di ricerca
 let searchInputReference = document.getElementById("searchIcon");
 let input;
 
-// collego l'evento click alla "button" per l'input search 
+// collego l'evento click alla "button" per l'input search
 searchInputReference.addEventListener("click", (e) => {
   e.preventDefault();
-  
+
   // piccola animazione per far comparire il campo di input da icona a text input in dissolvenza. . .
   let inpuText = document.createElement("input");
   inpuText.style.borderRadius = "30px";
@@ -31,15 +30,12 @@ let fadeAnimate = `@keyframes slideIn {
   }
 }`;
 
-
-let inputFade = document.createElement('style');
+let inputFade = document.createElement("style");
 inputFade.innerHTML = fadeAnimate;
 document.head.appendChild(inputFade);
 
-
 // FUNZIONE myShop per renderizzare i le cards dei prodotti nella Home Page
 const myShop = function () {
-  
   fetch(myApi, {
     method: "GET",
     headers: {
@@ -56,14 +52,13 @@ const myShop = function () {
         console.log(shopElements);
         let shop = document.getElementById("shopList");
         shop.innerHTML +=
-        
-        // CARDS BOOTSTRAP
-        `
+          // CARDS BOOTSTRAP
+          `
           <div class="card">
               <img src="${shopElements.imageUrl}" class="card-img-top img-fluid" alt="">
              <div class="card-body">
-                <h5 class="card-title text-md">${shopElements.name}</h5>
-                <p class="card-text mx-3 px-2 text-md">${shopElements.description}</p>
+                <h5 class="card-title">${shopElements.name}</h5>
+                <p class="card-text mx-3 px-2">${shopElements.description}</p>
                 <p>${shopElements.price} €</p>
                 <div>
                  <a href="details.html?eventId=${shopElements._id}" class="btn">Details</a>
